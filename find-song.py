a301d1e4699ddda540b4e3625977ba3357011d80
Michael Felix <65558188+mike-fmh@users.noreply.github.com>
2022-12-03 16:27:54 -0500

    v2.1

--- find-song.py
+++ find-song.py
@@ -6,6 +6,9 @@ import misc
 from acr_ratehandler import Ratehandler
 
 
+REC_LENGTH = 10  # how much secs to sample from each video by default
+
+
 class Bot:
     def __init__(self, account, reddit_auth, ratehandler):
         self.auth = reddit_auth
@@ -13,117 +16,125 @@ class Bot:
         self.rh = ratehandler
         self.files_to_del = []
 
+    def handle_context(self, msg):
+        ctx, video_url = None, None
+        if msg.was_comment:
+            try:
+                # check for parent comment, if it has a link it'll override the submission audio
+                if str(msg.parent().author) != self.acc_info.user and "https://" in str(msg.parent().body):
+                    # if there's no parent comment this will error out of the try statement
+                    words = str(msg.parent().body).replace("\n", " ").replace(",", " ").split(" ")
+                    for word in words:
+                        if ("youtu" in word and "https://" in word) or "twitch.tv" in word or "v.redd.it" in word or "tiktok.com" in word:  # make sure the parent has a compatible link
+                            ctx, video_url = "link_parent", word
+                            break
+            except AttributeError:  # if the comment has no parent
+                # it raises AttributeError: 'Submission' object has no attribute 'body'
+                if "https://" in str(msg.body):  # if the original message has a link
+                    words = str(msg.body).replace("\n", " ").replace(",", " ").split(" ")
+                    for word in words:
+                        if "https://" in word:
+                            ctx, video_url = "link_comment", word
+                            break
+                elif "https://" in str(msg.submission.selftext) and (("u/" + self.acc_info.user) in msg.body or ":" in msg.body):
+                    # else, assume they want the submission audio, or video from submission text
+                    words = str(msg.submission.selftext).split(" ")
+                    for word in words:
+                        if "https://" in word:
+                            ctx, video_url = "selftxt_link", word
+                            break
+                elif ("u/" + self.acc_info.user) in str(msg.body).lower() or ":" in msg.body:
+                    # else, assume they want the submission's video audio
+                    video_url = str(msg.submission.url)
+                    ctx = "video_submission"
+                else:
+                    print(msg.body, "> no context, skipping")
+                    msg.mark_read()
+                    raise misc.NoContext
+                return ctx, video_url
+        else:
+            # for replies to PMs made by the bot
+            parent_pm = self.auth.inbox.message(msg.parent_id[3:])  # this gets the first pm in the thread
+            if parent_pm.author == self.acc_info.user:  # check that the bot sent the first message in the pm thread
+                words = str(parent_pm.body).replace("\n", " ").replace(",", " ").split(" ")
+                for word in words:
+                    if ("youtu" in word and "https://" in word) or "twitch.tv" in word or "v.redd.it" in word or "tiktok.com" in word:  # make sure the parent has a compatible link
+                        ctx, video_url = "link_comment", word
+                        break
+        return ctx, video_url
+
+    def find_timestamps(self, msg, video_url):
+        start = -1
+        to = -1
+        skip = False
+        msg_words = str(msg.body).split(" ")  # get timestamp from original msg
+        for word in msg_words:
+            if "-" in word:  # if given start and end (0:30-0:50 or 30-50 formats)
+                word_split = word.split("-")
+                try:
+                    if ":" in word_split[0]:  # 0:30 format
+                        start = misc.timestamptoSec(word_split[0])
+                    else:  # 30 format (just plain seconds)
+                        start = int(word_split[0])
+                except misc.NoTimeStamp:
+                    if "u/" + self.acc_info.user not in str(msg.body).lower():
+                        skip = True
+                        break
+                except:
+                    pass
+                try:
+                    if ":" in word_split[-1]:  # 0:30 format
+                        to = misc.timestamptoSec(word_split[-1])
+                    else:  # 30 format (just plain seconds)
+                        to = int(word_split[-1])
+                except (misc.NoTimeStamp, ValueError):
+                    if "u/" + self.acc_info.user not in str(msg.body).lower():
+                        skip = True
+                        break
+            else:  # else, assume they gave just the starting point
+                try:
+                    if ":" in word:  # 0:30 format
+                        start = misc.timestamptoSec(word)
+                    else:  # 30 format (just plain seconds)
+                        start = int(word)
+                except:
+                    pass
+        if skip and "u/" + self.acc_info.user not in str(msg.body).lower() and "https://" not in str(msg.body).lower():
+            # bot shouldn't reply to this message
+            msg.mark_read()
+            raise misc.NoContext
+        video_url = misc.clear_formatting(video_url)
+        if start == -1:
+            if "youtu" in video_url and "https://" in video_url:
+                start = misc.get_yt_link_time(video_url)
+            else:
+                start = 0
+        if to == -1:
+            to = start + REC_LENGTH
+        return start, to, video_url
+
     def mentions_reply(self):
         reqs = []
         while True:
             for file in self.files_to_del:
-                # self.files_to_del format: [[filename, time_when_to_del], ...]
+                # each file downloaded to check audio will be deleted in this for loop
+                # if 10 minutes have passed since they were downloaded
                 try:
-                    if time.time() > file[1]:
+                    if file[1] < time.time():
                         os.remove(file[0])
-                        print(f"deleted file {file[0]}")
                         self.files_to_del.remove(file)
                 except:
                     pass
             try:
                 for msg in self.auth.inbox.unread():
-                    ctx = None
                     try:
-                        if "https://" in str(msg.body):  # if the original message has a link
-                            words = str(msg.body).replace("\n", " ").replace(",", " ").split(" ")
-                            for word in words:
-                                if "https://" in word:
-                                    ctx, video_url = "link_comment", word
-                                    break
-                        else:
-                            try:  # check if the parent comment (if there is any) contains a youtube link
-                                if str(msg.parent().author) != self.acc_info.user and "https://" in str(msg.parent().body):  # if there's no parent comment it will error out into the except statement
-                                    words = str(msg.body).replace("\n", " ").replace(",", " ").split(" ")
-                                    for word in words:
-                                        if ("youtu" in video_url and "https://" in video_url) or "twitch.tv" in word or "v.redd.it" in word  or "tiktok.com" in word:  # make sure the parent has a compatible link
-                                            ctx, video_url = "link_parent", word
-                                            break
-                                else:
-                                    raise Exception("no link")
-                                    # if parent comment exists but doesn't have a link,
-                                    # or it was left by the bot, jump to except statement
-
-                            except:
-                                # if the try statement fails
-                                # assume they want the submission audio, or video from submission text
-                                if "https://" in str(msg.submission.selftext) and "u/" + self.acc_info.user in msg.body:
-                                    # is there a link in submission selftext?
-                                    words = str(msg.submission.selftext).split(" ")
-                                    for word in words:
-                                        if "https://" in word:
-                                            ctx, video_url = "selftxt_link", word
-                                            break
-                                elif "u/" + self.acc_info.user in str(msg.body).lower():
-                                    # use submission's audio
-                                    video_url = str(msg.submission.url)
-                                    ctx = "video_submission"
-                                else:
-                                    # bot wasn't mentioned in the comment, or no link/video found
-                                    msg.mark_read()
-                                    continue
-
-                        start = -1
-                        to = -1
-                        skip = False
-                        msg_words = str(msg.body).split(" ")  # get timestamp from original msg
-                        for word in msg_words:
-                            if "-" in word:  # if given start and end (0:30-0:50 or 30-50 formats)
-                                word_split = word.split("-")
-                                try:
-                                    if ":" in word_split[0]:  # 0:30 format
-                                        start = misc.timestamptoSec(word_split[0])
-                                    else:  # 30 format (just plain seconds)
-                                        start = int(word_split[0])
-                                except misc.NoTimeStamp:
-                                    if "u/" + self.acc_info.user not in str(msg.body).lower():
-                                        # bot wasn't mentioned in the comment
-                                        skip = True
-                                        break
-                                except:
-                                    pass
-                                try:
-                                    if ":" in word_split[-1]:  # 0:30 format
-                                        to = misc.timestamptoSec(word_split[-1])
-                                    else:  # 30 format (just plain seconds)
-                                        to = int(word_split[-1])
-                                except (misc.NoTimeStamp, ValueError):
-                                    # the current word is not a timestamp, or it has ":" but no numbers
-                                    if "u/" + self.acc_info.user not in str(msg.body).lower():
-                                        # bot wasn't mentioned in the comment
-                                        skip = True
-                                        break
-                            else:  # else, assume they gave just the starting point
-                                try:
-                                    if ":" in word:  # 0:30 format
-                                        start = misc.timestamptoSec(word)
-                                    else:  # 30 format (just plain seconds)
-                                        start = int(word)
-                                except misc.NoTimeStamp:
-                                    skip = True
-                                    break
-                                except:
-                                    pass
-                        if skip and "u/" + self.acc_info.user not in str(msg.body).lower() and "https://" not in str(msg.body).lower():
-                            # this comment isn't for the bot to reply to
-                            msg.mark_read()
+                        try:
+                            ctx, video_url = self.handle_context(msg)
+                            start, to, video_url = self.find_timestamps(msg, video_url)
+                        except misc.NoContext:
                             continue
-                        video_url = misc.clear_formatting(video_url)
-                        if start == -1:
-                            if "youtu" in video_url and "https://" in video_url:
-                                start = misc.get_yt_link_time(video_url)
-                                # does the youtube link have &t=
-                            else:
-                                start = 0
-                        if to == -1:
-                            to = start + 30
-
                         if "v.redd.it" in video_url:
-                            output_file = misc.download_reddit(video_url + "/DASH_audio.mp4")
+                            output_file = misc.download_reddit(video_url)
                         elif "twitch.tv" in video_url:
                             output_file = misc.download_twitchclip(video_url)
                         elif "tiktok" in video_url:
@@ -132,46 +143,37 @@ class Bot:
                             try:
                                 output_file = misc.download_yt(video_url)
                             except misc.TooManyReqs:
-                                reqs.append([video_url, msg, time.time(), start, to, ctx, 0])
-                                # youtube ratelimited us, lets come back to this comment later
+                                # ratelimited by youtube, come back to this request later
+                                reqs.append([video_url, msg, time.time(), start, to, ctx, 0])   # if youtube gives too many reqs error, store videos and context for future reply
                                 msg.mark_read()
                                 continue
                             except:
-                                # other exceptions, skip the comment for good
                                 msg.mark_read()
                                 continue
                         else:
                             try:
                                 output_file = misc.download_reddit(video_url)
-                                # we've checked for supported video websites
-                                # for other websites lets try brute forcing downloading the video
-                                # to see if it works
                             except:
                                 raise misc.NoVideo
-
                         try:
                             data = misc.identify_audio(file=output_file, start_sec=start, end_sec=to, ratehandler=self.rh)
                         except misc.NoValidKey:
-                            print("no valid keys left!")
                             msg.mark_read()
                             continue
                         self.queue_del_file(output_file, 600)
                         if msg.was_comment:
-                            if data['msg'] == "error":
-                                # don't comment "no song found", private message the OP
+                            if data['msg'] == "error" and msg.subreddit != "NameThatSong" and msg.subreddit != "WhatsThisSong":
+                                # by default don't reply on error, pm instead
                                 self.auth.redditor(str(msg.author)).message("No song found", misc.create_response(data, ctx, video_url, start, to) + "\n\n^(This is a response to your comment \"" + msg.body + "\" in r/" + str(msg.subreddit) + ")")
                             else:
                                 try:
-                                    # if song found, reply to the comment
                                     msg.reply(misc.create_response(data, ctx, video_url, start, to))
                                 except:
                                     try:
-                                        # if can't comment, private message the OP
                                         self.auth.redditor(str(msg.author)).message("I couldn't reply to your comment, here's a PM instead", misc.create_response(data, ctx, video_url, start, to) + "\n\n^(This is a response to your comment \"" + msg.body + "\" in r/" + str(msg.subreddit) + ")")
                                     except:
                                         pass
                         else:
-                            # if received a private message, respond to the sender
                             self.auth.redditor(str(msg.author)).message(str(misc.sectoMin(start)) + "-" + str(misc.sectoMin(to)), misc.create_response(data, ctx, video_url, start, to) + "\n\n^(This is a response to your PM \"" + msg.body + "\")")
                     except:
                         pass
@@ -180,28 +182,26 @@ class Bot:
                 pass
 
             try:
-                # handle the comments we marked as "respond to later"
                 for context in reqs:
-                    if time.time() - context[2] > 1800:
+                    # retry requests that were ratelimited by youtube
+                    if time.time() - context[2] > 1800:  # for youtube ratelimits retry 30 minutes
                         video_url = context[0]
                         msg = context[1]
                         start, to, ctx = context[3], context[4], context[5]
                         try:
-                            output_file = misc.download_part_yt(video_url, start, to)
-                        except misc.RateLimit:
+                            output_file = misc.download_yt(video_url)
+                        except misc.TooManyReqs:
                             context[6] += 1
                             context[2] = time.time()
                             continue
-
                         try:
                             data = misc.identify_audio(file=output_file, start_sec=start, end_sec=to, ratehandler=self.rh)
                         except misc.NoValidKey:
-                            print("no valid keys left!")
                             msg.mark_read()
                             continue
                         self.queue_del_file(output_file, 600)
                         if msg.was_comment:
-                            if data['msg'] == "error":
+                            if data['msg'] == "error" and msg.subreddit != "NameThatSong" and msg.subreddit != "WhatsThisSong":
                                 self.auth.redditor(str(msg.author)).message("No song found", misc.create_response(data, ctx, video_url, start, to) + "\n\n^(This is a response to your comment \"" + msg.body + "\" in r/" + str(msg.subreddit) + ")")
                             else:
                                 try:
@@ -216,7 +216,7 @@ class Bot:
             except:
                 pass
 
-    def queue_del_file(self, filepath: str, how_long_to_wait: int):
+    def queue_del_file(self, filepath, how_long_to_wait):
         self.files_to_del.append([filepath, time.time() + how_long_to_wait])
 
 
